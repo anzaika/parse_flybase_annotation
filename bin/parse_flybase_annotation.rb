396bde9377ebbfba9c79d68257066f14b1a41e83
Andrey Zaika <anzaika@gmail.com>
2011-11-19 20:59:02 +0400

    add bin

---
+++ bin/parse_flybase_annotation.rb
@@ -0,0 +1,6 @@
+require_relative '../lib/parse_flybase_annotation'
+
+File.file?(ARGV[0]) or
+  raise 'You should pass valid path to flybase annotation file'
+
+ParseFlybaseAnnotation.parse(ARGV[0])
