e4cf25b3daa92eb29bcfa57d29a7c632e10a791a
Andrey Zaika <anzaika@gmail.com>
2011-11-20 16:28:21 +0400

    change parse to parse_and_upload

--- bin/parse_flybase_annotation.rb
+++ bin/parse_flybase_annotation.rb
@@ -3,4 +3,4 @@ require '../lib/parse_flybase_annotation'
 File.file?(ARGV[0]) or
   raise 'You should pass valid path to flybase annotation file'
 
-ParseFlybaseAnnotation.parse(ARGV[0])
+ParseFlybaseAnnotation.parse_and_upload(ARGV[0])
