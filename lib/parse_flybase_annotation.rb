e127c714aa58f11176b88d4185ec0e4691a5e145
Andrey Zaika <anzaika@gmail.com>
2011-11-24 11:14:27 +0400

    require bundler/setup

--- lib/parse_flybase_annotation.rb
+++ lib/parse_flybase_annotation.rb
@@ -1,6 +1,8 @@
 lib_dir = File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(lib_dir) unless $LOAD_PATH.include?(lib_dir)
 
+require 'bundler/setup'
+
 require 'my_ruby_extensions'
 
 require "parse_flybase_annotation/main"
